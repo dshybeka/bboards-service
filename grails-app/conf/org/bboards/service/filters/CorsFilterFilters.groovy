2db6ded1cff6bf155344597753a2250372998f2c
Dzianis Shybeka <dzianis_shybeka@epam.com>
2015-01-18 22:20:45 +0300

    Updated CorsFilterFilters.groovy

--- grails-app/conf/org/bboards/service/filters/CorsFilterFilters.groovy
+++ grails-app/conf/org/bboards/service/filters/CorsFilterFilters.groovy
@@ -5,9 +5,13 @@ class CorsFilterFilters {
     def filters = {
         all(controller:'*', action:'*') {
             before = {
+
+                log.debug "Regust origin header is " + request.getHeader("Origin")
+
                 def isLocalhost = request.getHeader("Origin").equals("http://localhost:8080") // TODO: move to Config.groovy
                 def isBservice = request.getHeader("Origin").contains("bservice-bboards.rhcloud.com")
                 if (isLocalhost || isBservice) {
+                    log.debug "Set header Access-Control-Allow-Origin"
                     response.setHeader("Access-Control-Allow-Origin", "*")
                     response.setHeader('Access-Control-Allow-Methods', 'POST, PUT, GET, HEAD, OPTIONS')
                 }
