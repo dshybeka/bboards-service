bf1cdca54f19b59e191fa1697a8e3d3b03235e23
Dzianis Shybeka <dzianis_shybeka@epam.com>
2015-01-18 22:57:22 +0300

    Updated Cors filter to support simple http requests

--- grails-app/conf/org/bboards/service/filters/CorsFilters.groovy
+++ grails-app/conf/org/bboards/service/filters/CorsFilters.groovy
@@ -8,8 +8,8 @@ class CorsFilters {
 
                 log.debug "Regust origin header is " + request.getHeader("Origin")
 
-                def isLocalhost = request.getHeader("Origin").equals("http://localhost:8080") // TODO: move to Config.groovy
-                def isBservice = request.getHeader("Origin").contains("bfront-bboards.rhcloud.com")
+                def isLocalhost = request.getHeader("Origin")?.equals("http://localhost:8080") // TODO: move to Config.groovy
+                def isBservice = request.getHeader("Origin")?.contains("bfront-bboards.rhcloud.com")
                 if (isLocalhost || isBservice) {
                     log.debug "Set header Access-Control-Allow-Origin"
                     response.setHeader("Access-Control-Allow-Origin", "*")
