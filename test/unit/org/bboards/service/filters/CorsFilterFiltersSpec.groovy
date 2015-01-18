4ea7e3cbda5a509ff3ca86b1a5e863563a75a7a9
Dzianis Shybeka <dzianis_shybeka@epam.com>
2015-01-18 22:23:08 +0300

    Added test for filter

---
+++ test/unit/org/bboards/service/filters/CorsFilterFiltersSpec.groovy
@@ -0,0 +1,17 @@
+package org.bboards.service.filters
+
+import grails.test.mixin.Mock
+import spock.lang.Specification
+
+@Mock(CorsFilterFilters)
+class CorsFilterFiltersSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
